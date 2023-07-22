38e2307ee474d4be7bc6fe407074499b3b76a33d
Pankaj <pgarg9355@gmail.com>
2023-07-22 23:02:51 +0530

    level-3

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-require('dotenv');
+require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const bcrypt = require('bcrypt');
 const encrpty=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 app.use(express.static("public"));
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
-mongoose.connect("mongodb+srv://${process.env.ID}:${process.env.Password}@cluster0.kgiclwb.mongodb.net/Userdb",{useNewUrlParser:true});
+mongoose.connect(`mongodb+srv://${process.env.ID}:${process.env.Password}@cluster0.kgiclwb.mongodb.net/Userdb`,{useNewUrlParser:true});
 // const userSchema={
 //     email:String,
 //     password:String
@@ -24,8 +25,7 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
-const secret=process.env.Secret;
-userSchema.plugin(encrpty,{secret:secret,encryptedFields: ['password']});
+userSchema.plugin(encrpty,{secret:process.env.secret,encryptedFields: ['password']});
 
 const User=new mongoose.model("User",userSchema);
 
@@ -43,7 +43,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
 const newUser=new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
 });
 newUser.save().then(()=>{
     res.render("secrets");
@@ -55,7 +55,7 @@ newUser.save().then(()=>{
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}).then((foundUser) => {     
         if(foundUser){
